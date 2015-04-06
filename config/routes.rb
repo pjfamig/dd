71e8a74b8fead322b4336dc0803d72246a268266
Paul Famiglietti <p0seid0n_@hotmail.com>
2015-04-05 20:51:02 -0700

    Finish login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Dd::Application.routes.draw do
+  get "sessions/new"
   root 'static_pages#about'
 
-  get 'about' => 'static_pages#about'
-  get 'rules' => 'static_pages#rules'
-  get 'jobs' => 'static_pages#jobs'
-  get 'register'  => 'users#new'
+  get 'about'       => 'static_pages#about'
+  get 'rules'       => 'static_pages#rules'
+  get 'jobs'        => 'static_pages#jobs'
+  get 'register'    => 'users#new'
+  get    'login'    => 'sessions#new'
+  post   'login'    => 'sessions#create'
+  delete 'logout'   => 'sessions#destroy'
   
   resources :users
 end
